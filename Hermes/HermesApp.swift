efb83b3c0bc079857e131f7a7305ba3412804873
DoniyorAkramovich <berd.doniyor@gmail.com>
2022-06-27 07:23:03 +0500

    Initial Commit

---
+++ Hermes/HermesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HermesApp.swift
+//  Hermes
+//
+//  Created by Doniyor on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct HermesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
